37586c359485145267d3ac367b9b19a21f08e56c
coderkoala <iamtribulation@outlook.com>
2019-07-13 10:30:51 +0545

    Refactoring code, finalizing. Squashing all commits.

---
+++ scrapper.py
@@ -0,0 +1,131 @@
+from getter import rawGetter
+from getter import isResponse
+from bs4 import BeautifulSoup
+import os
+import re
+import csv
+
+def append(tail):
+    #list_questions is for questions
+    #list_answer_tuple is for options
+    #list_right_tuple is for correct option
+    list_questions = list()
+    list_answer_tuple = list()
+    list_right_tuple = list()
+    '''getting the requests
+    '''
+    i_html = rawGetter(tail)
+    html = BeautifulSoup(i_html, 'html.parser')
+
+    if(html != None):
+        mother_crawl = html.find_all("span", attrs={"class": "qa_list"})
+        right = list()
+        questions = list()
+        '''
+        Start experiment'''
+        # table = html.find_all("div", attrs={"class": "qa_list"})
+        # for td in table: 
+        #     print('\n','\n','\n','\n')
+        #     if "google" in td.text:
+        #         continue
+        #     #Temp var for just taking in the data
+        #     newcsv = list()
+        #     #Praise no consistency for regex of input
+        #     d = td.find_all("span", attrs={"class":"option"})
+        #     for din in d:
+        #         dtext = din.text[3:].strip()
+        #         newcsv.append(dtext)
+        #     print(newcsv)
+
+        # exit()
+        # 
+        '''
+        Exit experiment'''
+        ###
+        # Questions
+        ###
+        #ul class='options_list clearfix'
+        element_question = html.find_all('ul', attrs= {"class":"options_list clearfix"})
+        for e in element_question:
+            questions.append(e.previousSibling)
+        
+        ###
+        # Answers
+        ###
+        element_span = html.find_all("span", attrs={"class":"answer option"})
+        for iterator_ele in element_span:
+            lis = iterator_ele.find_all("a")
+            for iterator_lis in lis:
+                right.append(iterator_lis.text[0])
+
+        #checking to see if empty. If empty, skip page
+        if(questions != [] and right != []):
+            list_questions = questions
+            list_right_tuple = right
+        else: 
+            return
+        #to iterate through
+        iteration = 0
+        #print(len(list_questions[5]))
+
+        table = html.find_all("div", attrs={"class": "qa_list"})
+        for td in table:
+            if "google" in td.text:
+                continue
+            if(iteration<=len(list_questions)-1):
+                #Temp var for just taking in the data
+                newcsv = list()
+                newcsv.append(list_questions[iteration])
+                newcsv.append(list_right_tuple[iteration])
+                #Praise no consistency for regex of input
+                d = td.find_all("span", attrs={"class":"option"})
+                for din in d:
+                    dtext = din.text[3:].strip()
+                    newcsv.append(dtext)
+                print('[APPEND]: ',newcsv)
+                list_scraped.append(newcsv)
+                iteration = iteration + 1
+            else:
+                break
+            """            
+            for opt in options:
+                if(count <= 4):
+                    count = count + 1
+                    list_answer_tuple.append(opt.text)
+                else:
+                    newcsv.append(list_questions[iteration])
+                    newcsv.extend(list_answer_tuple)
+                    newcsv.append(list_right_tuple[iteration])
+                    list_scraped.append(newcsv)
+                    print('[APPEND]: ',list_questions[iteration], list_answer_tuple, list_right_tuple[iteration])
+                    count = 1
+                    iteration = iteration + 1
+                    list_answer_tuple = list()
+                    newcsv = list()
+                    continue"""
+
+list_final = [['Question','Answer','Option A', 'Option B', 'Option C', 'Option D', 'Option E']]
+#used to collect the objects
+list_scraped = list()
+latter = list()
+input_file_name = input("[NAME] Output to save: ")
+index = input("[PASTE] url to crawl: ")
+#inquiry = input("[RECURSION]Try automatic child pages? [y/n]: ")
+inquiry = "n"
+if inquiry == "y" or inquiry == "Y":
+    latter = map(str,input("[/ 1 2 3]Please enter the sub urls to crawl: \n").split())
+    for ip in latter:
+        append(index + ip)
+else:
+    append(index)
+list_final.extend(list_scraped)
+
+#write it to an spreadsheet readable format(csv)
+with open(input_file_name+ '.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(list_final)
+    csvFile.close()
+try:
+    os.system("start excel "+input_file_name+".csv && exit")
+except:
+    pass
